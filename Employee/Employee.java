0ba10c96ea53b13f83b41a7e2c3192cfac80fe5d
Vladislav-Romanov <Vladislav-Romanov@users.noreply.github.com>
2018-06-24 15:31:33 +0300

    -

--- Employee/Employee.java
+++ Employee/Employee.java
@@ -2,26 +2,31 @@ package ru.ncedu.java.tasks;
 
 import java.util.Scanner;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.io.*;
+import java.util.Arrays;
 
-public class Employee {
+
+public class Employee  {
 //private field
-private String second_name;     //фамилия
-private String first_name;	//имя
-private String job;             //должность
-private String salary;             //зарплата
-ArrayList<String> Emp_List;
+private String second_name=null;     //фамилия
+private String first_name=null;	//имя
+private String job=null;             //должность
+private int  salary=0;             //зарплата
+ArrayList<Employee> Emp_List = new ArrayList<Employee>();
+String job_f = null;
+
 
 //конструктор
-Employee(String second_name,String first_name,String job,String salary) { 
+public Employee(String second_name,String first_name,String job,int salary) { 
     this.second_name = second_name;
     this.first_name = first_name;
     this.job = job;
     this.salary = salary;
 }
 
-public String setEmployee(){
+
+
+//заполнение контейнера данными с клавиатуры
+public void setEmployee(){
     
     System.out.println();
     Scanner sc = new Scanner(System.in);
@@ -34,40 +39,77 @@ public String setEmployee(){
     
     System.out.println("Введите должность сотрудника: ");
     job = sc.nextLine();
-     
-    System.out.println("Введите зарплату сотрудника : ");
-    salary = sc.nextLine();
     
-    return second_name + " " + first_name + " " + job + " " + salary;
+    
+    System.out.println("Введите зарплату сотрудника : ");
+    salary = sc.nextInt();
+    
+    Emp_List.add(new Employee(second_name,first_name,job,salary));
 }
 
-//заполнение контейнера данными с клавиатуры
-public void  addList(){
-ArrayList<String> Emp_List = new ArrayList<>();
-Emp_List.add(setEmployee());
+
+public String getSecond_name(){
+	return second_name;
 }
+
+public String getFirst_name(){
+	return first_name;
+}
+
+public String getJob(){
+	return job;
+}
+
+public int getSalary(){
+	return salary;
+}
+
+
 
 //вывод информации о сотрудниках на экран
 public void display(){
-for (int i = 0; i < Emp_List.size(); i++) {
-    System.out.println(Emp_List.get(i));
+   for(int i=0;i<Emp_List.size();i++){
+    System.out.println(Emp_List.get(i).toString());
+   }
 }
+
+public String toString()
+{ 
+    return "Фамилия-"+this.second_name+" Имя-"+this.first_name+" Должность-"+this.job+" Зарплата-"+this.salary;
 }
 
 //поиск сотрудников, состоящих на заданной должности
-public void found(String found_job) {
-    try {      
-    int in=Emp_List.indexOf(found_job);
-    System.out.println(Emp_List.get(in));
-    
-    } catch (NullPointerException e){
-    System.out.println("Робота не выбрана");    
-    }
-          
+public void job(String job_f){
+    boolean f = false;
+    for (int i=0; i<Emp_List.size();i++) {
+        f=Emp_List.get(i).toString().contains(job_f);
+        if(f == true){
+            System.out.println(Emp_List.get(i).toString());
+            f = false;
+        }
+    } 
+
 }
 
+
 //сортировку списка сотрудников по полю фамилия и по полю зарплата.
-public void sort(){
-    
+
+
+
+
+
+
 }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
